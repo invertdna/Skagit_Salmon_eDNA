605539b0f3a930ccda5c5a10cd229d6c30bf03e2
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-12-16 14:56:24 -0800

    incorporate individual bottles into plot

--- Analysis/qpcr_vs_fish.R
+++ Analysis/qpcr_vs_fish.R
@@ -2,11 +2,6 @@
 
 
 #-------------------------------------------------------------------------------
-# convert quantities to picograms per microliter (makes plotting better)
-qpcr_data[,"QuantBackCalc"] <- qpcr_data[,"QuantBackCalc"]*1000
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
 # get median dna per bottle across the three replicate qPCR reactions
 dna_by_bottle <- split(qpcr_data[,"QuantBackCalc"], qpcr_data[,"template_name"])
 
@@ -50,7 +45,13 @@ dna_by_event <- split(qpcr_data[,"QuantBackCalc"], qpcr_data[,"event_id"])
 # get sum of counts of chinook by sampling event
 chinook_by_event <- sapply( split(
   catch_chinook[,3:5], catch_chinook[,"event_id"]
-  ), sum)
+), sum)
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# compute log for better plotting
+chinook_by_event_log <- log(chinook_by_event + 1)
 #-------------------------------------------------------------------------------
 
 
@@ -66,7 +67,7 @@ plot_dat_bottle <- data.frame(
   event_id = bottle_to_event, 
   bottle   = names(dna_by_bottle_med), 
   dna      = dna_by_bottle_med,
-  fish     = chinook_by_event[bottle_to_event]
+  fish     = chinook_by_event_log[bottle_to_event]
 )[!is.na(bottle_to_event),] # remove rows of standards
 #-------------------------------------------------------------------------------
 
@@ -74,7 +75,7 @@ plot_dat_bottle <- data.frame(
 #-------------------------------------------------------------------------------
 # arrange for plotting and modelling
 dna  <- dna_by_event_med[events_in_both]
-fish <- chinook_by_event[events_in_both]
+fish <- chinook_by_event_log[events_in_both]
 
 plot_dat <- data.frame(
   event = names(unlist(dna)), 
@@ -86,14 +87,14 @@ plot_dat <- data.frame(
 
 #-------------------------------------------------------------------------------
 # assess the fit of a linear model
-model_linear <- lm(dna ~ log(fish + 1), data = plot_dat)
+model_linear <- lm(dna ~ fish, data = plot_dat)
 model_linear_summary <- summary(model_linear)
 
 x_pred <- 0:max(plot_dat$fish)
 
 con95 <- predict(
     object   = model_linear,
-    newdata  = data.frame(fish = log(x_pred + 1)),
+    newdata  = data.frame(fish = x_pred),
     interval = "confidence",
     level    = 0.95
   )
@@ -105,17 +106,20 @@ con95 <- predict(
 # requires plot_dat, with columns named "dna" and "fish"
 plot_name <- "DNA_by_seine_chinook"
 
-EXPORT <- FALSE
+EXPORT <- TRUE
 
 if(!exists("legend_text")){legend_text <- list()}
 legend_text[plot_name] <- {"
 Concentration of Chinook Salmon DNA plotted against Chinook Salmon captured in beach seines at 10 sites in Skagit Bay.
 At each site, 2 beach seines and 3-4 water samples were collected. 
 The total number of Chinook across seine surveys is plotted here. 
-DNA was extracted from each bottle and used as template in 3 independent qPCRs; 
-the median of these is taken, and the site concentration is taken as the median of these estimates.
+DNA was extracted from each bottle and used as template in 3 independent qPCRs, 
+the median of these is taken as the DNA concentration of each bottle (open gray circles);
+the site concentration is taken as the median of these estimates (closed black circles).
 A linear model demonstrates that the intercept cannot be distinguished from 0 (p = 0.596), 
-while the number of fish (log(x+1)) was a significant predictor of DNA concentration (p = 0.000762).
+while the number of fish (log(x+1)) was a significant predictor of median DNA concentration (p = 0.000762). 
+While only one point appears at the origin (0,0), this represents two sites 
+at which 0 Chinook were captured in seines and none of the water samples (8 total) contained Chinook DNA.
 "}
 
 if(EXPORT){
@@ -127,19 +131,23 @@ if(EXPORT){
 
 par(mar = c(4,5,1,1))
 plot(
-  x = log(plot_dat$fish + 1), 
-  y = plot_dat$dna, 
+  x = plot_dat_bottle$fish, 
+  y = jitter(plot_dat_bottle$dna, factor = 0), 
+  pch = 19, lwd = 2, col = hsv(1, 0, 0.5), 
+  cex = 0.5, 
   axes = FALSE, 
-  xlab = "Total Chinook", 
+  xlab = "Total Chinook in Beach Seines", 
   ylab = expression(paste("Concentration Chinook DNA (pg/", mu, "L)", sep = "")), 
-  pch = 19, col = "black", 
   las = 1
   )
 
-# add points for each bottle
+abline(model_linear, col = "cornflowerblue", lwd = 2)
+
+# add medians
 points(
-  x = plot_dat_bottle$fish, 
-  y = plot_dat_bottle$dna
+  x = plot_dat$fish, 
+  y = plot_dat$dna, 
+  pch = 19, col = "black"
 )
 
 xaxis_ticks <- c(0, 5, 10, 25, 50, 100, 200)
@@ -151,7 +159,6 @@ axis(1,
 
 axis(2, las = 1)
 
-abline(model_linear, col = "cornflowerblue", lwd = 2)
 
 box()
 
