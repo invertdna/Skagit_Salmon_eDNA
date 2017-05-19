0699b3748b0559c7ab42dfa3a8c14c604be7c7d2
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-05-19 15:12:03 -0700

    move simulation

--- Analysis/simulate_count_data.R
+++ Analysis/simulate_count_data.R
@@ -1,13 +1,45 @@
-# simulate count data
 
-# source: http://stats.stackexchange.com/questions/9767/generating-over-dispersed-counts-data-with-serial-correlation
+# make up some data
+EXPORT <- FALSE
 
-rho_vals <- seq(from = 0.1, to = 0.8, by = 0.1)
-y <- list()
-for(i in 1:length(rho_vals)){
-N <- 100
-rho <- rho_vals[i]
-log.lambda <- 1 + arima.sim(model = list(ar=rho), n = N)
-y[[i]] <- rpois(N, lambda = exp(log.lambda))
+site_names <- sites$site_name
+
+species <- c(
+  "Oncorhynchus tshawytscha",
+  "Oncorhynchus nerka",
+  "Oncorhynchus kisutch",
+  "Oncorhynchus gorbuscha",
+  "Ammodytes hexapterus",
+  "Clupea pallasii",
+  "Engraulis mordax",
+  "Cymatogaster aggregata", 
+  "Gasterosteus aculeatus",
+  "Hypomesus pretiosus",
+  "Mallotus villosus"
+)
+
+season <- c("Summer") # , "Winter1", "Winter2"
+
+raw_counts <- integer()
+for(s in 1:length(season)){
+  for(i in 1:length(site_names)){
+    # simulate count data
+    N <- length(species)
+    rho <- 0.1
+    log.lambda <- 1 + arima.sim(model=list(ar=rho), n=N)
+    y <- rpois(N, lambda=exp(log.lambda))
+    raw_counts <- c(raw_counts, y)
+  }
 }
-boxplot(y)
+
+
+counts_net <- data.frame(
+  season  = rep(season, each = length(site_names)*length(species)),
+  site    = rep(rep(site_names, each = length(species)), each = length(season)),
+  species = rep(species, times = length(site_names)*length(season)),
+  count   = raw_counts
+)
+
+if(EXPORT){
+  write.csv(counts_net, file = file.path(data_dir, "counts_net_fake.csv"))
+}
