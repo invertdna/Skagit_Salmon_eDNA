f181b80bb613f71bcda319f8c6bd3b9df65ad082
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-06-27 13:13:06 -0700

    modify to pull data from google sheets

--- Analysis/load_water_samples.R
+++ Analysis/load_water_samples.R
@@ -1,17 +1,22 @@
 # load water samples
 #-------------------------------------------------------------------------------
+# requires an object "catch_metadata" ?does it?
 
-# requires an object "catch_metadata"
+library(googlesheets)
+library(data.table)
 
-water_file <- file.path(data_dir, "water_samples.csv")
+# water_file <- file.path(data_dir, "water_samples.csv")
+# water <- read.csv(water_file, stringsAsFactors = FALSE)
 
-water <- read.csv(water_file, stringsAsFactors = FALSE)
+env_samples_gs_key <- "1IzJG3jaZCNXu6GNtx0ltsyJn0D8bprWs6NcYXCTGg_A"
 
-water$site_abbr <- site_trans[match(water[,"site_name"], site_trans[,3]), 1]
+water <- data.table(gs_read(gs_key(env_samples_gs_key)))
+
+water <- merge(water, sites[, c("site_name", "Abbr")], by = "site_name", all.x = TRUE)
 
 water$event_id <- paste(
-  water$site_abbr, 
-  gsub("-", "", substr(water$date_collected, 3, 10)), sep = "-"
+  water$Abbr, 
+  format(water$date, "%y%m%d"), sep = "-"
   )
 
 
