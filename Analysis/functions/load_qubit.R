6c9fac072abdc29dc8c62f2f0bcbdbe07026d0d3
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-10-13 15:22:00 -0700

    make argument dna_type optional

--- Analysis/functions/load_qubit.R
+++ Analysis/functions/load_qubit.R
@@ -1,9 +1,11 @@
 # load qubit file
 
-load_qubit <- function(filepath, dna_type)
+load_qubit <- function(filepath, dna_type = NULL)
 {
   library(data.table)
   qubit <- fread(filepath)
-  qubit[,dna_type := dna_type]
+  if(!is.null(dna_type)){ # could use missing()
+  	qubit[,dna_type := dna_type]
+  }
   return(qubit)
 }
