3a2cd422fc577c2112df76324d591864e7e7079d
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-10-13 16:29:19 -0700

    correct column names and set out of range to 0

--- Analysis/functions/load_qubit.R
+++ Analysis/functions/load_qubit.R
@@ -7,5 +7,10 @@ load_qubit <- function(filepath, dna_type = NULL)
   if(!is.null(dna_type)){ # could use missing()
   	qubit[,dna_type := dna_type]
   }
+  colnames(qubit) <- make.names(colnames(qubit), unique = TRUE)
+  qubit[ Original.sample.conc. == "Out of range", Original.sample.conc. := "0"]
+  qubit[ Qubit..tube.conc. == "Out of range", Qubit..tube.conc. := "0"]
+  qubit[ , Original.sample.conc. := as.numeric(Original.sample.conc.)]
+  qubit[ , Qubit..tube.conc. := as.numeric(Qubit..tube.conc.)]
   return(qubit)
 }
