7d317368b0cc8d178bca91b886785f7b01052f28
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-02 15:52:07 -0700

    add sample sheet generation

---
+++ Analysis/sample_sheet_gen.R
@@ -0,0 +1,11 @@
+################################################################################
+setwd("~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-170712/setup")
+
+samplefile <- "2017-07-12_samples.txt"
+samplenames <- read.table(samplefile, colClasses = "character")[,1]
+plated <- plate_it(samplenames, plate_size = 96, reps = 4)
+EXPORT <- FALSE
+if(EXPORT){
+  write.csv(plated[[1]], file = "sample_sheet.csv", row.names = FALSE, quote = FALSE)
+  write.csv(plated[[2]], file = "plate_layout.csv")
+}
