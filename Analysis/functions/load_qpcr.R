73ae061b82607c4930a36088fcc266474b9f6123
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-03 15:37:33 -0700

    fix order of arguments

--- Analysis/functions/load_qpcr.R
+++ Analysis/functions/load_qpcr.R
@@ -16,7 +16,7 @@
 #' 
 #' @export
 load_qpcr <- function(
-  std_conc = 1, qpcr_data_file, sample_sheet_file = NULL, 
+  qpcr_data_file, std_conc = 1, sample_sheet_file = NULL, 
   drop_cols = TRUE, drop_100 = TRUE, quant1000 = TRUE, drop_rows = TRUE)
 {
   library(data.table)
