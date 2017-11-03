120910313475552a4d6ee1f1ab80d3d6eec1cfd3
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-03 16:18:01 -0700

    add junk

--- Analysis/qpcr_checkup.R
+++ Analysis/qpcr_checkup.R
@@ -1,31 +1,26 @@
-results3 <- load_qpcr(
-  std_conc = 1, 
+R3 <- load_qpcr(
   qpcr_data_file = "../Data/qpcr/CKCO3-170712/results/results_table.txt", 
-  sample_sheet_file = "../Data/qpcr/CKCO3-170712/setup/sample_sheet.csv"
+  sample_sheet_file = "../Data/qpcr/CKCO3-170712/setup/sample_sheet.csv", 
+  std_conc = 1
 )
 
-R3 <- results3
 R3[, logQuant := log(Quantity)]
 
-mycols <- gghue(2)
-mycols <- hsv(h = c(1, 1/8, 0.6), s = 0.7)
+mycols <- hsv(h = c(1/8, 0.6), s = 0.7)
 
 stdplot()
 with(R3, points(logQuant, Ct, col = mycols[as.numeric(as.factor(R3[,Task]))], lwd = 2))
-with(R3, plot(logQuant, Ct, col = mycols[as.numeric(as.factor(R3[,Task]))], lwd = 2))
+with(R3, plot(logQuant, Ct, col = mycols[as.numeric(as.factor(Task))], lwd = 2))
 plot(R3[,Ct], col = mycols[as.numeric(as.factor(R3[,Task]))], lwd = 3)
 
-R3 <- R3[Task != "NTC", ]
-
-with(R3, plot(logQuant, Ct, col = mycols[as.numeric(as.factor(Task))], lwd = 2))
+with(R3, stripchart(Ct ~ template_name,  method = 'jitter', las = 1))
+with(R3, abline(h = 1:length(unique(template_name)), col = grey(0, 0.2), lty = 2))
 with(R3, 
   plot(
     as.numeric(as.factor(template_name)), 
     Ct, 
     col = mycols[as.numeric(as.factor(Task))], lwd = 2
 ))
-R3[,as.numeric(as.factor(template_name))]
-R3
 abline(v = 1:20, col = grey(0.8, alpha = 0.5))
 R3[, .N, by = as.numeric(as.factor(template_name))]
 
