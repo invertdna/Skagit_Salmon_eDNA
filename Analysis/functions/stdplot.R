d919813befe0e9d81227703439c1f17a3b423c71
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-08 10:24:32 -0800

    actually use the argument to set ylim

--- Analysis/functions/stdplot.R
+++ Analysis/functions/stdplot.R
@@ -4,7 +4,7 @@ stdplot <- function(cycles = 45, dil.factors = 10^(-7:0)){
   plot(
     x = log10(seqx), 
     y = seq(from = 0, to = cycles, length.out = length(seqx)), 
-    ylim = c(0, 45), 
+    ylim = c(0, cycles), 
     type = "n", axes = FALSE, ann = FALSE
   )
   axis(1, at = log10(seqx), labels = seqx)
