822cc843a20e68ecf079710ef01ed0b5e977b2ed
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-11-08 17:19:14 -0800

    add tip for accomodating weird file

--- Analysis/qpcr_checkup.R
+++ Analysis/qpcr_checkup.R
@@ -25,3 +25,26 @@ abline(v = 1:20, col = grey(0.8, alpha = 0.5))
 R3[, .N, by = as.numeric(as.factor(template_name))]
 
 with(R3, points(logQuant, Ct, col = mycols[as.numeric(as.factor(DT[,Task]))], lwd = 2))
+
+
+# To deal with weird data files (multiple reporters/assays):
+thefile <- "/Users/jimmy.odonnell/qpcr_data/Jimmy_Batch_Export/CYAGCYTB-170829-A/results/CYAGCYTB-170829-A_result.txt"
+
+thefile <- normalizePath(thefile)
+
+START <- 11
+STOP <- 35
+thecall <- paste0("sed -n ", '"', START, ",", STOP, 'p" ', thefile)
+fread(thecall)
+dat <- load_qpcr(thecall)
+
+stdat <- dat[Task == "Standard", ]
+
+model_dat <- list()
+model_dat[[1]] <- stdat
+
+model_out <- list()
+for(i in 1:length(model_dat)){
+  model_out[[i]] <- lm(formula = Ct ~ log10(Quantity), data = model_dat[[i]])
+}
+# GOTO qpcr_analysis for plotting
