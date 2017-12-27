a0738c5f300f55ebdbc2be6a450abd1b6b4f6757
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-27 13:58:00 -0800

    functionalize

--- Analysis/load_dna.R
+++ Analysis/load_dna.R
@@ -1,8 +1,8 @@
-
-library(googlesheets)
-library(data.table)
-
-DNA_sheet_key <- "1tWeJMDBqVydnRPsW_ql65Xd0PDLYWf8mQIP7T0fNmvc"
-#"1rXBXtXW7Ov1Z_LMZo_2G8Wjo_XStsl9WerGKdnylBGw"
-
-DNA <- data.table(gs_read(gs_key(DNA_sheet_key)))
+################################################################################
+load_dna <- function(sheet_id = "1tWeJMDBqVydnRPsW_ql65Xd0PDLYWf8mQIP7T0fNmvc"){
+  library(googlesheets)
+  library(data.table)
+  #"1rXBXtXW7Ov1Z_LMZo_2G8Wjo_XStsl9WerGKdnylBGw"
+  DNA <- data.table(gs_read(gs_key(sheet_id)))
+  return(DNA)
+}
