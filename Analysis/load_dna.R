39dc0b40ae207b006a1565ed4c33af552094ce38
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-27 12:40:01 -0800

    change DNA google sheet id

--- Analysis/load_dna.R
+++ Analysis/load_dna.R
@@ -2,6 +2,7 @@
 library(googlesheets)
 library(data.table)
 
-DNA_sheet_key <- "1rXBXtXW7Ov1Z_LMZo_2G8Wjo_XStsl9WerGKdnylBGw"
+DNA_sheet_key <- "1tWeJMDBqVydnRPsW_ql65Xd0PDLYWf8mQIP7T0fNmvc"
+#"1rXBXtXW7Ov1Z_LMZo_2G8Wjo_XStsl9WerGKdnylBGw"
 
 DNA <- data.table(gs_read(gs_key(DNA_sheet_key)))
