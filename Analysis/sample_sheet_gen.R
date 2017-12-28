101d0fa881ddb898c6e04ac81b7971ff02d57b1d
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-28 15:21:46 -0800

    organize

--- Analysis/sample_sheet_gen.R
+++ Analysis/sample_sheet_gen.R
@@ -15,8 +15,11 @@ setwd("~/GoogleDrive/Data/qpcr_data/Jimmy_Batch_Export/CKCO3-170830")
 tmp <- fread("results/CKCO3-170830_result.txt")
 
 plot(tmp[,Quantity], log = 'y')
-plot(log1p(tmp[,Quantity]), log = 'y')
+tmp[,Quantity]
+tmp[Quantity == 0,]
+plot(tmp[Task == 'Standard',Ct])
 abline(h = 0)
+
 samplefile <- "setup/samples.txt" # "2017-07-12_samples.txt"
 samplenames <- read.table(samplefile, colClasses = "character")[,1]
 plated <- plate_it(samplenames, plate_size = 96, reps = 4)
