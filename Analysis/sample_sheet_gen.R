d06f0ec2884a6d7a61d57d6743db1c2895905fb0
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-28 11:01:44 -0800

    figuring

--- Analysis/sample_sheet_gen.R
+++ Analysis/sample_sheet_gen.R
@@ -9,3 +9,19 @@ if(EXPORT){
   write.csv(plated[[1]], file = "sample_sheet.csv", row.names = FALSE, quote = FALSE)
   write.csv(plated[[2]], file = "plate_layout.csv")
 }
+
+
+setwd("~/GoogleDrive/Data/qpcr_data/Jimmy_Batch_Export/CKCO3-170830")
+tmp <- fread("results/CKCO3-170830_result.txt")
+
+plot(tmp[,Quantity], log = 'y')
+plot(log1p(tmp[,Quantity]), log = 'y')
+abline(h = 0)
+samplefile <- "setup/samples.txt" # "2017-07-12_samples.txt"
+samplenames <- read.table(samplefile, colClasses = "character")[,1]
+plated <- plate_it(samplenames, plate_size = 96, reps = 4)
+EXPORT <- FALSE
+if(EXPORT){
+  write.csv(plated[[1]], file = "sample_sheet.csv", row.names = FALSE, quote = FALSE)
+  write.csv(plated[[2]], file = "plate_layout.csv")
+}
