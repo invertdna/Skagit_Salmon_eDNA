d6ffeca8faa715f49c2369a97315bc174f9d43f2
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-28 06:14:03 -0800

    fix broken link

--- Analysis/functions/load_cleaned.R
+++ Analysis/functions/load_cleaned.R
@@ -1,6 +1,7 @@
 ################################################################################
 load_cleaned <- function(sheet_id = 
-  "1d32mxfDkdnCf9ggN0BPdzyvvvGuj9obD_pry2iFlp_0"){
+  "1wX6tRSRVmm-xaDQ1Eh-3F8Po10vbfePBM_eWd4MCeEA"){
+  # was: 1d32mxfDkdnCf9ggN0BPdzyvvvGuj9obD_pry2iFlp_0
   library(googlesheets)
   library(data.table)
   cleaned <- data.table(gs_read(gs_key(sheet_id)))
