26eb3facd23bc4aab890443eecd28299fd00aa69
Jimmy O'Donnell <jodonnellbio@gmail.com>
2018-02-28 10:44:51 -0800

    add qpcr loading function

---
+++ Analysis/qpcr_load.R
@@ -0,0 +1,54 @@
+#-------------------------------------------------------------------------------
+# Load qPCRs into a list
+#-------------------------------------------------------------------------------
+
+res <- list()
+
+#-------------------------------------------------------------------------------
+# 1
+res[[1]] <- load_qpcr(
+  qpcr_data_file = "~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-161209/results/results_table.txt", 
+  sample_sheet_file = "~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-161209/setup/sample_sheet.csv", 
+  std_conc = 9.36)
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# 2
+cols_to_remove <- c('no_lab_error', 'note')
+res[[2]] <- load_qpcr(
+  qpcr_data_file = "~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-161214/results/results_table.txt", 
+  sample_sheet_file = "~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-161214/setup/sample_sheet.csv", 
+  std_conc = 9.36
+)[ 
+  # exclude samples with lab error:
+  no_lab_error == TRUE , ][, 
+  (cols_to_remove) := NULL]
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# 3
+res[[3]] <- load_qpcr(
+  qpcr_data_file = '~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-170712/results/results_table.txt', 
+  sample_sheet_file = '~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-170712/setup/sample_sheet.csv', 
+  std_conc = 1)
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# 4
+res[[4]] <- load_qpcr(
+  qpcr_data_file = "~/GoogleDrive/Data/qpcr_data/Jimmy_Batch_Export/CKCO3-170830/results/CKCO3-170830_result.txt", 
+  sample_sheet_file = "~/Projects/Skagit_Salmon_eDNA/Data/qpcr/CKCO3-170830/setup/sample_sheet.csv", 
+  std_conc = 1)[,well_id := NULL]
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# 5
+res[[5]] <- load_qpcr(
+  qpcr_data_file = "~/GoogleDrive/Data/qpcr_data/CKCO3-180102/results/CKCO3-180102_result.txt",
+  sample_sheet_file = "~/GoogleDrive/Data/qpcr_data/CKCO3-180102/setup/sample_sheet.csv", 
+  std_conc = 1)[,Quantity := Quantity/4.78]
+#-------------------------------------------------------------------------------
