6059fb4ae75bd513a87fa848b804fe12bf829205
Jimmy O'Donnell <jodonnellbio@gmail.com>
2018-03-07 15:08:36 -0800

    fix typo

--- Analysis/qpcr_load.R
+++ Analysis/qpcr_load.R
@@ -48,7 +48,7 @@ res[[4]] <- load_qpcr(
 #-------------------------------------------------------------------------------
 # 5: note the std quantity was 4.78 and was entered into the software on the qpcr machine.
 res[[5]] <- load_qpcr(
-  qpcr_data_file = "../Data/qpcr_data/CKCO3-180102/results/CKCO3-180102_result.txt",
+  qpcr_data_file = "../Data/qpcr/CKCO3-180102/results/CKCO3-180102_result.txt",
   sample_sheet_file = "../Data/qpcr/CKCO3-180102/setup/sample_sheet.csv", 
   std_conc = 1)[,Quantity := Quantity/4.78]
 #-------------------------------------------------------------------------------
