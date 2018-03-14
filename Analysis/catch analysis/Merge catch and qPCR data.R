bd3a76b43afad22ba81b0203ce122916e6b7af9a
Jimmy O'Donnell <jodonnellbio@gmail.com>
2018-03-14 09:54:51 -0700

    ole and jimmy tweaking stuff

--- Analysis/catch analysis/Merge catch and qPCR data.R
+++ Analysis/catch analysis/Merge catch and qPCR data.R
@@ -2,21 +2,26 @@
 
 
 # Load catch information from seines
-source("/Users/ole.shelton/GitHub/Skagit_Salmon_eDNA/Analysis/catch analysis/parse catch data.R")
+source("parse catch data.R")
 
 # Load up water samples and information from qPCR
-setwd("/Users/ole.shelton/GitHub/Skagit_Salmon_eDNA/Analysis/")
+setwd("..")
 source("0_load_project.R")
 source("qpcr_load.R")
 
-data_dir <- "/Users/ole.shelton/GitHub/Skagit_Salmon_eDNA/Data/"
+data_dir <- "../Data/"
 water <- load_water_samples()
 
 # we are interested in the objects "water" and "res[[7]]" which corresponds to the qpcr run on 180302
 head(water)
 head(res[[7]])
 
+res[[6]][,note := NULL]
+res[[7]][,note := NULL]
+
+dat <- as.data.frame(rbindlist(res))
 dat <- as.data.frame(res[[7]])
+
 sort(unique(dat$template_name))
 sort(unique(water$lab_label))
 names(water)
@@ -28,8 +33,5 @@ samp.run <- left_join(water,dat.summ,by=c("lab_label"="template_name"))
 temp <- samp.run %>% filter(is.na(n.rep)==F) %>% group_by(site_name, date) %>% summarize(samp = length(n.rep)) %>%
           dcast(.,date~site_name,value.var=c("samp")) %>% as.data.frame()
 
-
-
-
-
+samp.run %>% filter(site_name %in% c("Mariners Bluff","Brown Point","Dugualla Bluff")) %>% select(date,site_name,n.rep,lab_label)
 
